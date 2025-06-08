16d4a8918a9cc6bdd6ab63d44198b675cc7cf4f2
Diah082 <152632332+Diah082@users.noreply.github.com>
2025-06-09 00:46:31 +0700

    Update insshws.sh

--- sshws/insshws.sh
+++ sshws/insshws.sh
@@ -5,13 +5,13 @@ file_path="/etc/handeling"
 # Cek apakah file ada
 if [ ! -f "$file_path" ]; then
     # Jika file tidak ada, buat file dan isi dengan dua baris
-    echo -e "Newbie Server Connected\nGreen" | sudo tee "$file_path" > /dev/null
+    echo -e "FREE SC NEWBIE\nGreen" | sudo tee "$file_path" > /dev/null
     echo "File '$file_path' berhasil dibuat."
 else
     # Jika file ada, cek apakah isinya kosong
     if [ ! -s "$file_path" ]; then
         # Jika file ada tetapi kosong, isi dengan dua baris
-        echo -e "Newbie Server Connected\nGreen" | sudo tee "$file_path" > /dev/null
+        echo -e "FREE SC NEWBIE\nGreen" | sudo tee "$file_path" > /dev/null
         echo "File '$file_path' kosong dan telah diisi."
     else
         # Jika file ada dan berisi data, tidak lakukan apapun
